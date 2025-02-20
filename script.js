58aa595fb2d7b93def540ee2a3595e9545319ff2
iM Mustafa <im262897tt@gmail.com>
2025-02-20 10:02:41 -0800

    start functionally

---
+++ script.js
@@ -0,0 +1,10 @@
+// Utility function to generate a random hex color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
